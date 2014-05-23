1fdb308dc38ca9ce687affba0f54eba54ddba330
vharseko <vharseko@openam.org.ru>
2014-05-23 18:45:21 +0400

    -

--- src/test/java/com/iplanet/services/comm/server/Test_PLLRequestServlet.java
+++ src/test/java/com/iplanet/services/comm/server/Test_PLLRequestServlet.java
@@ -11,6 +11,6 @@ public class Test_PLLRequestServlet {
 		assertEquals("ACMDM.*\" preferredNamingURL=\"\"><GetNamingProfile>",PLLRequestServlet.fix("ACMDM.*\" preferredNamingURL=\"Z^AsplayName|passport_displayname\">  \" <GetNamingProfile>", null));
 		assertEquals("ACMDM.*\" preferredNamingURL=\"https://am2.psb.inside.ru/auth\"><GetNamingProfile>",PLLRequestServlet.fix("ACMDM.*\" preferredNamingURL=\"https://am2.psb.inside.ru/auth\"https://bank.ru:443\" and \"https://bank \r\n<GetNamingProfile>", null));
 		assertEquals("ACMDM.*\" preferredNamingURL=\"https://am2.psb.inside.ru/auth\"><GetNamingProfile>",PLLRequestServlet.fix("ACMDM.*\" preferredNamingURL=\"https://am2.psb.inside.ru/auth\">\r\n<GetNamingProfile>", null));
-		assertEquals("ACMDM.*\" preferredNamingURL=\"\"><GetNamingProfile>",PLLRequestServlet.fix("ACMDM.*\" preferredNamingURL=\"https:x/\"> \r\n<GetNamingProfile>", null));
+		assertEquals("ACMDM.*\" preferredNamingURL=\"\"><GetNamingProfile>",PLLRequestServlet.fix("ACMDM.*\" preferredNamingURL=\"httpsd:x/\"> \r\n<GetNamingProfile>", null));
 	}
 }
