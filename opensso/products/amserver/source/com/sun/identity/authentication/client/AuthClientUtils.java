3217942be66f8bb83822dde59deb0d6eaf7a7618
maxim.thomas <maxim.thomas@gmail.com>
2014-07-07 21:36:00 +0400

    cookie domain fix

--- opensso/products/amserver/source/com/sun/identity/authentication/client/AuthClientUtils.java
+++ opensso/products/amserver/source/com/sun/identity/authentication/client/AuthClientUtils.java
@@ -1416,9 +1416,10 @@ public class AuthClientUtils {
         if (!allDomains.isEmpty()) {
             for (Iterator it = allDomains.iterator(); it.hasNext(); ) {
                 domain = (String)it.next();
-                if (domain.indexOf(host) >= 0 || host.indexOf(domain) > 0) {
+                //if (domain.indexOf(host) >= 0 || host.indexOf(domain) > 0) {
+                if (host.endsWith(domain)||".".concat(host).endsWith(domain)) 
                     domains.add(domain);
-                }
+                
             }
         }
 
