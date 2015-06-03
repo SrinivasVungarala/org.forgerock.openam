22709e7f1408d9a847003b82befb645b42dae309
vharseko <vharseko@openam.org.ru>
2015-06-03 23:07:26 +0300

    fix reverse comapre

--- opensso/products/amserver/source/org/forgerock/openam/session/service/DestroyOldestAction.java
+++ opensso/products/amserver/source/org/forgerock/openam/session/service/DestroyOldestAction.java
@@ -55,7 +55,7 @@ public class DestroyOldestAction implements QuotaExhaustionAction {
 	    {
 	        public int compare( Map.Entry<K, V> o1, Map.Entry<K, V> o2 )
 	        {
-	            return (o1.getValue()).compareTo( o2.getValue() );
+	            return (o2.getValue()).compareTo( o1.getValue() );
 	        }
 	    } );
 	
