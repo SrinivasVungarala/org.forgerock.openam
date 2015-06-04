69867d693f278909e205e2888386c61f6485c79e
vharseko <vharseko@openam.org.ru>
2015-06-04 12:10:44 +0300

    replace appSSOToken outside processSessionResponseException

--- openam/openam-core/src/main/java/com/iplanet/dpro/session/Requests.java
+++ openam/openam-core/src/main/java/com/iplanet/dpro/session/Requests.java
@@ -122,6 +122,8 @@ public class Requests {
                 }
                 // send request again
                 sres = pllSender.sendPLLRequest(svcurl, sreq);
+                if (sres.getException() != null && session.getContext() instanceof SSOToken)
+                	appSSOToken=(SSOToken)session.getContext();
             }
         } catch (Exception e) {
             throw new SessionException(e);
